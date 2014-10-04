f6d8603322bd48721c56407e0421d341c82fa4b7
dy131 <2205866790@qq.com>
2014-10-04 21:56:51 +0800

    commit
    Signed-off-by: dy131 <2205866790@qq.com>

--- fhrweb/src/com/fhrweb/web/controller/LoginController.java
+++ fhrweb/src/com/fhrweb/web/controller/LoginController.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 
 /**
- * loginController
+ * Ê×Ò³
  * @author xingguo.lu
  * @date 2014-10-04 
  *
@@ -30,7 +29,9 @@ public class LoginController extends BaseController {
 		
 		ModelAndView mv = new ModelAndView("tennis/index");
 		
-		//System.out.println("dasfsda");
+		System.out.println("dasfsda");
+		System.out.println("ddddddddddddd");
+		System.out.println("ddddddddddddd");
 		System.out.println("ddddddddddddd");
 		
 		//mv.addObject("newsList", newsList);

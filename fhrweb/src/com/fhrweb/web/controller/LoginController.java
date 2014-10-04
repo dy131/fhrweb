d956ad8b1b855c600d0b3c21b406c4ef7dcaba5c
dy131 <2205866790@qq.com>
2014-10-04 21:47:32 +0800

    提交

--- fhrweb/src/com/fhrweb/web/controller/LoginController.java
+++ fhrweb/src/com/fhrweb/web/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController extends BaseController {
 		
 		ModelAndView mv = new ModelAndView("tennis/index");
 		
-		//System.out.println("dasfsda");
+		System.out.println("dasfsda");
 		
 		//mv.addObject("newsList", newsList);
 		return null;
